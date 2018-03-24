51a1006447206e880f1421db00f2dcbee34fe4cf
jayantrane <jayantrane0811@gmail.com>
2018-03-24 12:48:12 +0530

    First
    Initial Commit

---
+++ home.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link rel="stylesheet" href="css/home.css">
+
+    <title>IPL</title>
+</head>
+<body>
+
+    <!-- This is Top and right navigation bar. Edit only links -->
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="navbar-header">
+          <!-- <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button> -->
+          <a class="navbar-brand" href="#">IPL</a>
+        </div>
+
+        <!-- <div class="collapse navbar-collapse s">-->
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </ul>
+        <!--</div>/.nav-collapse --> 
+
+    </div><!--/.navbar -->
+
+    <div class="row-offcanvas row-offcanvas-left">
+      <div id="sidebar" class="sidebar-offcanvas">
+          <div class="col-md-12">
+                <h3>Menu</h3>
+                <ul class="nav  nav-stacked">
+                  <li class="active"><a href="#">Home</a></li>
+                  <li><a href="#">Points Table</a></li>
+                  <li><a href="#">Schedule</a></li>
+                  <li><a href="#">Player Stats</a></li>
+                  <li><a href="#">Teams</a></li>
+                  <li><a href="#">About Us</a></li>
+                </ul>
+            </div>
+        </div>
+    
+
+        <!-- End of Navigation bars -->
+
+        <!-- Start Your Code Here -->
+
+        <div id="main">
+
+
+        </div>
+
+    </div>
+  
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
