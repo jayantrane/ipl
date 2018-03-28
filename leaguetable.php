5b6ffc3df207c4547b411b69eb8a1feac22249a1
PrabhuPratik27 <prabhupratik27@gmail.com>
2018-03-28 16:18:32 +0530

    Changes in the leaguetable.php interfacing with mysql done

--- leaguetable.php
+++ leaguetable.php
@@ -1,6 +1,28 @@
 <?php 
 session_start();
+
+$mysqli=mysqli_connect("localhost","root","","ipldb");
+
+$sql=array();	
+
+for($i=1;$i<9;$i++){
+	$sql[$i]="select * from leaguetable where id = $i ";
+}
+
+$result=array();
+
+for($i=1;$i<9;$i++){
+	$result[$i]=mysqli_query($mysqli,$sql[$i]);
+}
+
+$values=array();
+
+for($i=1;$i<9;$i++){
+	$values[$i]=mysqli_fetch_array($result[$i],MYSQLI_NUM);
+}
+
 ?>
+
 <!DOCTYPE html>
 
 <html>
@@ -44,82 +66,82 @@ session_start();
 
 						<tr >
 							<td>1</td>
-							<td><img src="css/images/mi.png" class="miimage"><span class="tab">Mumbai Indians</span></img></td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
+							<td><img src="css/images/mi.png" class="miimage"><span class="tab"><?= $values[1][1]?></span></img></td>
+							<td><?= $values[1][2] ?></td>
+							<td><?= $values[1][3] ?></td>
+							<td><?= $values[1][4] ?></td>
+							<td><?= $values[1][5] ?></td>
+							<td><?= $values[1][6] ?></td>
 						</tr>
 
 						<tr >
 							<td>2</td>
-							<td><img src="css/images/csk.png" class="miimage"><span class="tab">Chennai Super Kings</span></img></td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
+							<td><img src="css/images/csk.png" class="miimage"><span class="tab"><?= $values[2][1] ?></span></img></td>
+							<td><?= $values[2][2] ?></td>
+							<td><?= $values[2][3] ?></td>
+							<td><?= $values[2][4] ?></td>
+							<td><?= $values[2][5] ?></td>
+							<td><?= $values[2][6] ?></td>
 						</tr>
 
 						<tr >
 							<td>3</td>
-							<td><img src="css/images/rcb.png" class="miimage"><span class="tab">Royal Challengers Banglore</span></img></td><
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
+							<td><img src="css/images/rcb.png" class="miimage"><span class="tab"><?= $values[3][1] ?></span></img></td><
+							<td><?= $values[3][2] ?></td>
+							<td><?= $values[3][3] ?></td>
+							<td><?= $values[3][4] ?></td>
+							<td><?= $values[3][5] ?></td>
+							<td><?= $values[3][6] ?></td>
 						</tr>
 
 						<tr >
 							<td>4</td>
-							<td><img src="css/images/srh.png" class="miimage"><span class="tab">Sunrisers Hyderabad</span></img></td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
+							<td><img src="css/images/srh.png" class="miimage"><span class="tab"><?= $values[4][1] ?></span></img></td>
+							<td><?= $values[4][2] ?></td>
+							<td><?= $values[4][3] ?></td>
+							<td><?= $values[4][4] ?></td>
+							<td><?= $values[4][5] ?></td>
+							<td><?= $values[4][6] ?></td>
 						</tr>
 
 						<tr >
 							<td>5</td>
-							<td><img src="css/images/dd.png" class="miimage"><span class="tab">Delhi Daredevils</span></img></td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
+							<td><img src="css/images/dd.png" class="miimage"><span class="tab"><?= $values[5][1] ?></span></img></td>
+							<td><?= $values[5][2] ?></td>
+							<td><?= $values[5][3] ?></td>
+							<td><?= $values[5][4] ?></td>
+							<td><?= $values[5][5] ?></td>
+							<td><?= $values[5][6] ?></td>
 						</tr>
 
 						<tr >
 							<td>6</td>
-							<td><img src="css/images/kkr.png" class="miimage"><span class="tab">Kolkata Knight Riders</span></img></td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
+							<td><img src="css/images/kkr.png" class="miimage"><span class="tab"><?= $values[6][1] ?></span></img></td>
+							<td><?= $values[6][2] ?></td>
+							<td><?= $values[6][3] ?></td>
+							<td><?= $values[6][4] ?></td>
+							<td><?= $values[6][5] ?></td>
+							<td><?= $values[6][6] ?></td>
 						</tr>
 
 						<tr >
 							<td>7</td>
-							<td><img src="css/images/kxip.png" class="miimage"><span class="tab">Kings XI Punjab</span></img></td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
+							<td><img src="css/images/kxip.png" class="miimage"><span class="tab"><?= $values[7][1] ?></span></img></td>
+							<td><?= $values[7][2] ?></td>
+							<td><?= $values[7][3] ?></td>
+							<td><?= $values[7][4] ?></td>
+							<td><?= $values[7][5] ?></td>
+							<td><?= $values[7][6] ?></td>
 						</tr>
 
 						<tr >
 							<td>8</td>
-							<td><img src="css/images/rr.png" class="miimage"><span class="tab">Rajasthan Royals</span></img></td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
-							<td>0</td>
+							<td><img src="css/images/rr.png" class="miimage"><span class="tab"><?= $values[8][1] ?></span></img></td>
+							<td><?= $values[8][2] ?></td>
+							<td><?= $values[8][3] ?></td>
+							<td><?= $values[8][4] ?></td>
+							<td><?= $values[8][5] ?></td>
+							<td><?= $values[8][6] ?></td>
 						</tr>
 					</table>
 			</div>
