45fe5eeb62947b537c426f9e51c8f5e19a284577
jayantrane <jayantrane0811@gmail.com>
2018-03-29 12:40:04 +0530

    font awesome
    font awesome added

--- teaminfo.php
+++ teaminfo.php
@@ -22,8 +22,8 @@
 <html>
 	<head>
 	<meta name=viewport content="width=device-width, initial-scale=1"> 
-
-	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- Font Awesome -->
+    <link rel="stylesheet" href="css/font-awesome.css">
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- Your custom styles (optional) -->
