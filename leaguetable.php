2aa0aab76ee062dcdba45d98d740b4f12101dfab
PrabhuPratik27 <prabhupratik27@gmail.com>
2018-03-29 12:58:00 +0530

    Center alignment in laegue table nothing else

--- leaguetable.php
+++ leaguetable.php
@@ -55,7 +55,7 @@ for($i=1;$i<9;$i++){
 					<table class="table table-bordered table-striped table-hover table-container">
 
 						<tr>
-							<th>Id</th>
+							<th>Position</th>
 							<th>Name</th>
 							<th>MatchesPlayed</th>
 							<th>Wins</th>
@@ -66,7 +66,7 @@ for($i=1;$i<9;$i++){
 
 						<tr >
 							<td>1</td>
-							<td><?php $image1="css/images/".$values[1][7].".png" ; ?><img src="<?php echo $image1 ; ?>" class="miimage"><span class="tab"><?= $values[1][1]?></span></img></td>
+							<td class="name"><?php $image1="css/images/".$values[1][7].".png" ; ?><img src="<?php echo $image1 ; ?>" class="miimage"><span class="tab"><?= $values[1][1]?></span></img></td>
 							<td><?= $values[1][2] ?></td>
 							<td><?= $values[1][3] ?></td>
 							<td><?= $values[1][4] ?></td>
@@ -76,7 +76,7 @@ for($i=1;$i<9;$i++){
 
 						<tr >
 							<td>2</td>
-							<td><?php $image2="css/images/".$values[2][7].".png" ; ?><img src="<?php echo $image2 ; ?>" class="miimage"><span class="tab"><?= $values[2][1] ?></span></img></td>
+							<td class="name"><?php $image2="css/images/".$values[2][7].".png" ; ?><img src="<?php echo $image2 ; ?>" class="miimage"><span class="tab"><?= $values[2][1] ?></span></img></td>
 							<td><?= $values[2][2] ?></td>
 							<td><?= $values[2][3] ?></td>
 							<td><?= $values[2][4] ?></td>
@@ -86,7 +86,7 @@ for($i=1;$i<9;$i++){
 
 						<tr >
 							<td>3</td>
-							<td><?php $image3="css/images/".$values[3][7].".png" ; ?><img src="<?php echo $image3 ; ?>" class="miimage"><span class="tab"><?= $values[3][1] ?></span></img></td><
+							<td class="name"><?php $image3="css/images/".$values[3][7].".png" ; ?><img src="<?php echo $image3 ; ?>" class="miimage"><span class="tab"><?= $values[3][1] ?></span></img></td><
 							<td><?= $values[3][2] ?></td>
 							<td><?= $values[3][3] ?></td>
 							<td><?= $values[3][4] ?></td>
@@ -96,7 +96,7 @@ for($i=1;$i<9;$i++){
 
 						<tr >
 							<td>4</td>
-							<td><?php $image4="css/images/".$values[4][7].".png" ; ?><img src="<?php echo $image4 ; ?>" class="miimage"><span class="tab"><?= $values[4][1] ?></span></img></td>
+							<td class="name"><?php $image4="css/images/".$values[4][7].".png" ; ?><img src="<?php echo $image4 ; ?>" class="miimage"><span class="tab"><?= $values[4][1] ?></span></img></td>
 							<td><?= $values[4][2] ?></td>
 							<td><?= $values[4][3] ?></td>
 							<td><?= $values[4][4] ?></td>
@@ -106,7 +106,7 @@ for($i=1;$i<9;$i++){
 
 						<tr >
 							<td>5</td>
-							<td><?php $image5="css/images/".$values[5][7].".png" ; ?><img src="<?php echo $image5 ; ?>" class="miimage"><span class="tab"><?= $values[5][1] ?></span></img></td>
+							<td class="name"><?php $image5="css/images/".$values[5][7].".png" ; ?><img src="<?php echo $image5 ; ?>" class="miimage"><span class="tab"><?= $values[5][1] ?></span></img></td>
 							<td><?= $values[5][2] ?></td>
 							<td><?= $values[5][3] ?></td>
 							<td><?= $values[5][4] ?></td>
@@ -116,7 +116,7 @@ for($i=1;$i<9;$i++){
 
 						<tr >
 							<td>6</td>
-							<td><?php $image6="css/images/".$values[6][7].".png" ; ?><img src="<?php echo $image6 ; ?>" class="miimage"><span class="tab"><?= $values[6][1] ?></span></img></td>
+							<td class="name"><?php $image6="css/images/".$values[6][7].".png" ; ?><img src="<?php echo $image6 ; ?>" class="miimage"><span class="tab"><?= $values[6][1] ?></span></img></td>
 							<td><?= $values[6][2] ?></td>
 							<td><?= $values[6][3] ?></td>
 							<td><?= $values[6][4] ?></td>
@@ -126,7 +126,7 @@ for($i=1;$i<9;$i++){
 
 						<tr >
 							<td>7</td>
-							<td><?php $image7="css/images/".$values[7][7].".png" ; ?><img src="<?php echo $image7 ; ?>" class="miimage"><span class="tab"><?= $values[7][1] ?></span></img></td>
+							<td class="name"><?php $image7="css/images/".$values[7][7].".png" ; ?><img src="<?php echo $image7 ; ?>" class="miimage"><span class="tab"><?= $values[7][1] ?></span></img></td>
 							<td><?= $values[7][2] ?></td>
 							<td><?= $values[7][3] ?></td>
 							<td><?= $values[7][4] ?></td>
@@ -136,7 +136,7 @@ for($i=1;$i<9;$i++){
 
 						<tr >
 							<td>8</td>
-							<td><?php $image8="css/images/".$values[8][7].".png" ; ?><img src="<?php echo $image8 ; ?>" class="miimage"><span class="tab"><?= $values[8][1] ?></span></img></td>
+							<td class="name"><?php $image8="css/images/".$values[8][7].".png" ; ?><img src="<?php echo $image8 ; ?>" class="miimage"><span class="tab"><?= $values[8][1] ?></span></img></td>
 							<td><?= $values[8][2] ?></td>
 							<td><?= $values[8][3] ?></td>
 							<td><?= $values[8][4] ?></td>
