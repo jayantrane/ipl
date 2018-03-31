24de70529029554546afd4eccfa9d50556652cfa
PrabhuPratik27 <prabhupratik27@gmail.com>
2018-04-01 00:08:58 +0530

    Half Home Page

--- home.php
+++ home.php
@@ -3,6 +3,35 @@ session_start();
 if(empty($_SESSION['isLoggedIn']))
   $_SESSION['isLoggedIn'] = false;
 $_SESSION['onpage'] = 'home';
+
+$mysqli=mysqli_connect("localhost","root","","ipldb");
+
+$yep=mt_rand(1,8);
+
+$sql1="select * from teams where pid_teams='".$yep."' ";
+
+$result1=mysqli_query($mysqli,$sql1);
+
+$values=mysqli_fetch_array($result1);
+
+$image="css/images/".$values[0].".png";
+
+$yep2=mt_rand(1,56);
+
+$status=array("Not-Happened","Already-Happened");
+
+$sql2="select * from matches where pid_matches='".$yep2."' ";
+
+$result2=mysqli_query($mysqli,$sql2);
+
+$values1=mysqli_fetch_array($result2);
+
+$teams=array("","Mumbai Indians","Chennai Super Kings","Royal Challengers Bangalore","Kolkata Knight Riders","Sunrisers Hyderabad","Rajasthan Royals","Kings XI Punjab","Delhi Daredevils");
+
+
+$image1="css/images/".array_search(strtolower($values1[2]),array_map('strtolower', $teams)).".png";
+$image2="css/images/".array_search(strtolower($values1[3]),array_map('strtolower', $teams)).".png";
+
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +45,10 @@ $_SESSION['onpage'] = 'home';
     <link rel="stylesheet" href="css/mynavigation.css">
     <!-- Your custom styles (optional) -->
 
+    <link rel="stylesheet" href="css/home.css">
+    
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
     <title>IPL</title>
 </head>
 <body>
@@ -23,11 +56,38 @@ $_SESSION['onpage'] = 'home';
    
   <!-- always include one </div> elements before </body> -->
     <?php include 'mynavigation.php';?>
-        <div id='content' class="col-md-10 main">
 
+        <div id='content' class="col-md-10 main">
+          
+          <div class="row">
+
+            <div class="col-md-3">
+              <div class="card text-center">
+                  <img class="card-img-top" src="<?php echo $image ?>">
+                  <div class="card-body">
+                    <h5 class="card-title team-name"><?= $values[1] ?>
+                  </div>
+              </div>
+            </div>
+
+            <div class="col-md-1"></div>
+
+            <div class="col-md-6">
+              <div class="card text-center">
+                  <img class="card-img-top" src="<?php echo $image1 ?>" > <b class="vstext">v/s</b> 
+                  <img class="card-img-top" src="<?php echo $image2 ?>" >
+                  <div class="card-body">
+                    <h5 class="card-title team-name"><?= $values1[2] ?> v/s <?= $values1[3] ?>
+                    <h5 class="card-title team-name"><?= $values1[1] ?>
+                    <h5 class="card-title team-name"><?= $values1[4] ?>
+                    <h5 class="card-title team-name"><?= $status[$values1[5]] ?>
+                  </div>
+              </div>
+            </div>
+
+          </div>
 
         </div>
-    </div>
   
     <!-- SCRIPTS -->
     <!-- JQuery -->
@@ -49,7 +109,7 @@ $_SESSION['onpage'] = 'home';
                     $('a[aria-expanded=true]').attr('aria-expanded', 'false');
                 });
             });
-        </script>
+    </script>
 
 </body>
 </html>
