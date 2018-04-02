57d5600cbc6dbef076cdc8256627d23ab9431649
jayantrane <jayantrane0811@gmail.com>
2018-04-02 15:31:48 +0530

    Users Tables Updated

--- mysql/users.sql
+++ mysql/users.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 24, 2018 at 02:17 PM
--- Server version: 5.7.19
--- PHP Version: 5.6.31
+-- Host: 127.0.0.1
+-- Generation Time: Apr 02, 2018 at 09:54 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,22 +26,22 @@ SET time_zone = "+00:00";
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `username` varchar(60) DEFAULT NULL,
-  `password` varchar(60) DEFAULT NULL
+  `password` varchar(60) DEFAULT NULL,
+  `name` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`username`, `password`) VALUES
-('Pratik', 'Pratik'),
-('Jayant', 'Jayant'),
-('Siddhesh', 'Siddhesh'),
-('Onkar', 'Onkar');
-COMMIT;
+INSERT INTO `users` (`username`, `password`, `name`, `email`) VALUES
+('pratik', 'pratik', 'Pratik Prabhu', 'pratikprabhu@gmail.com'),
+('jayant', 'jayant', 'Jayant Rane', 'jayantrane@gmail.com'),
+('siddhesh', 'siddhesh', 'Siddhesh Patel', 'siddheshpatel@gmail.com'),
+('onkar', 'onkar', 'Onkar Bangar', 'onkarbangar@gmail.com');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
